418a775ede97eedd4d0358623fbcc7d194816400
lleung030 <lucasleung2593@gmail.com>
2023-08-07 13:56:21 -0700

    pages scrolldown

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,19 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import {MagnifyingGlassIcon} from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import SearchButton from "./SearchButton";
+import {
+  SearchSelect,
+  SearchSelectItem,
+  Select,
+  SelectItem,
+} from "@tremor/react";
 
 function Header() {
   return (
     <header>
-      <Link href='/ '>
+      <Link href="/ ">
         <Image
           src="https://links.papareact.com/208"
           alt="Logo"
@@ -19,13 +25,27 @@ function Header() {
 
       <div className="w-full md:max-w-2xl">
         <form action="">
-            <div className="flex items-center gap-2 w-full px-4">
-                <div className="flex items-center space-x-2 bg-white shadow-xl rounded-full border-0 px-6 py-4 flex-1">
-                    <MagnifyingGlassIcon className='h-5 w-5 text-gray-400' />
-                    <input type="text" name='searchTerm' placeholder='Search...' className="outline-none flex-1" />
-                </div>
-                <SearchButton />
+          <div className="flex items-center gap-2 w-full px-4">
+            <div className="flex items-center space-x-2 bg-white shadow-xl rounded-full border-0 px-6 py-4 flex-1">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+              <input
+                type="text"
+                name="searchTerm"
+                placeholder="Search..."
+                className="outline-none flex-1"
+              />
             </div>
+            <SearchButton />
+          </div>
+          <div>
+            <SearchSelect>
+              {[...Array(100)].map((_, i) => (
+                <SearchSelectItem key={i} value={(i + 1).toString()}>
+                  {(i + 1).toString()} pages
+                </SearchSelectItem>
+              ))}
+            </SearchSelect>
+          </div>
         </form>
       </div>
     </header>
