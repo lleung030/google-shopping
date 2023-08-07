dad6f2895b3255aa6815b09ab6c1d2d788711679
lleung030 <lucasleung2593@gmail.com>
2023-08-07 12:57:52 -0700

    pending status && useFormStatus

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,10 +1,17 @@
-'use client'
+"use client";
+
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
 function SearchButton() {
+  const { pending } = useFormStatus();
+
   return (
-<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full disabled:opacity-50 disabled:cursor-not-allowed">
-    Search
-</button>
-  )
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full disabled:opacity-50 disabled:cursor-not-allowed">
+      {pending && "Searching..."}
+      {!pending && <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />}
+    </button>
+  );
 }
 
-export default SearchButton
+export default SearchButton;
