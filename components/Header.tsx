53f72fd25a50a62388320a1b8f543ca24f5a3db2
lleung030 <lucasleung2593@gmail.com>
2023-08-07 12:41:46 -0700

    mangifying glass icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,32 @@
-'use client'
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import {MagnifyingGlassIcon} from '@heroicons/react/24/solid';
 
 function Header() {
   return (
-  <header>
-    <Image 
-        src='https://links.papareact.com/208'
-        alt='Logo'
-        width={150}
-        height={150}
-        className='object-contain mr-10 '
-    />
+    <header>
+      <Link href='/ '>
+        <Image
+          src="https://links.papareact.com/208"
+          alt="Logo"
+          width={150}
+          height={150}
+          className="object-contain mr-10 "
+        />
+      </Link>
 
-  </header>
-  )
+      <div>
+        <form action="">
+            <div>
+                <div>
+                    <MagnifyingGlassIcon className='h-5 w-5 text-gray-400' />
+                </div>
+            </div>
+        </form>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
