61e75318a596b5a7a400ce85a4f312810c56bdc5
lleung030 <lucasleung2593@gmail.com>
2023-08-09 09:14:31 -0700

    individual page with title & pictures

--- app/api/search/shopping/product/[id]/route.ts
+++ app/api/search/shopping/product/[id]/route.ts
@@ -1,46 +1,44 @@
 import { ProductData } from "@/typings";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-type Params = { params: {id: string}}
+type Params = { params: { id: string } };
 
-export async function GET(
-    request: Request,
-    {params: { id } } : Params) {
-    if(!id) {
-        return NextResponse.next(
-            new Response('MMissing product ID', {
-                status: 400,
-            })
-        );
-    }
+export async function GET(request: Request, { params: { id } }: Params) {
+  if (!id) {
+    return NextResponse.next(
+      new Response("MMissing product ID", {
+        status: 400,
+      })
+    );
+  }
 
-    const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Basic ${Buffer.from(
-                process.env.OXYLABS_USERNAME + ':' + process.env.OXYLABS_PASSWORD
-            ).toString('base64')}`
-        },
-    cache: 'no-store',
+  const response = await fetch("https://realtime.oxylabs.io/v1/queries", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${Buffer.from(
+        process.env.OXYLABS_USERNAME + ":" + process.env.OXYLABS_PASSWORD
+      ).toString("base64")}`,
+    },
+    cache: "no-store",
     body: JSON.stringify({
-        source: 'google_shopping_product',
-        domain: 'com',
-        query: id,
-        parse: true,
+      source: "google_shopping_product",
+      domain: "com",
+      query: id,
+      parse: true,
     }),
-    });
-    const data = await response.json();
+  });
+  const data = await response.json();
 
-    if (data.results.length === 0) {
-        return NextResponse.next(
-            new Response('No product found', {
-                status: 404,
-            })
-        );
-    }
+  if (data.results.length === 0) {
+    return NextResponse.next(
+      new Response("No product found", {
+        status: 404,
+      })
+    );
+  }
 
-    const productData: ProductData = data.results[0];
+  const productData: ProductData = data.results[0];
 
-    return NextResponse.json(productData);
+  return NextResponse.json(productData);
 }
--- app/shopping/product/[id]/page.tsx
+++ app/shopping/product/[id]/page.tsx
@@ -1,25 +1,73 @@
-import { getFetchUrl } from '@/lib/getFetchUrl';
-import { ProductData } from '@/typings';
-import { StarIcon } from '@heroicons/react/24/solid';
-import { notFound } from 'next/navigation';
-
-
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { ProductData } from "@/typings";
+import { StarIcon } from "@heroicons/react/24/solid";
+import { notFound } from "next/navigation";
 
 export const revalidate = 300;
 
 type Props = {
-    params: {
-        id: string;
-    }
-}
+  params: {
+    id: string;
+  };
+};
 
 async function ProductPage({ params: { id } }: Props) {
-    const response = await fetch(getFetchUrl(`api/shopping/product/${id}`));
+  const response = await fetch(getFetchUrl(`api/shopping/product/${id}`));
+  const productData = (await response.json()) as ProductData;
+
+  if (!productData.content.pricing) {
+    notFound();
+  }
+
   return (
-    <div>
-      
+    <div className="p-12 pt-0">
+      <h1 className="text-2xl">{productData.content.title}</h1>
+
+      {productData.content.reviews && (
+        <div className="flex space-x-1">
+          {[
+            ...Array.from({
+              length: Math.round(productData.content.reviews.rating),
+            }),
+          ].map((_, i) => (
+            <StarIcon key={i} className="h-5 w-5 text-yellow-500" />
+          ))}
+
+          {[
+            ...Array.from({
+              length: 5 - Math.round(productData.content.reviews.rating),
+            }),
+          ].map((_, i) => (
+            <StarIcon key={i} className="h-5 w-5 text-gray-200" />
+          ))}
+        </div>
+      )}
+
+      <section className="flex flex-col lg:flex-row mt-5 md:mt-0">
+        <div className="md:0-10 md:pl-0 mx-auto">
+          <div className="flex gap-4">
+            <img
+              className="h-80 w-80 border rounded-md object-contain"
+              src={productData.content.images?.full_size[0]}
+              alt=""
+            />
+            <div className="flex flex-col justify-between">
+              {productData.content.images?.full_size
+                .slice(1, 3)
+                .map((image) => (
+                  <img
+                    src={image}
+                    alt=""
+                    className="w-[9.5rem] h-[9.5rem] object-contain
+                        border rounded-md"
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default ProductPage
+export default ProductPage;
